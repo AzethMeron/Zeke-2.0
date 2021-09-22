abdf2e1dd8c275ba52cda84aa47e23f792983ad6
AzethM <meronentertainment@gmail.com>
2021-09-22 12:21:56 +0200

    Tests done serially

--- mode.py
+++ mode.py
@@ -1,16 +1,10 @@
 
-from multiprocessing import Process
-from multiprocessing import Queue
-
 import triggers
 import data
 
 ################################################################################
 
-data.NewGuildEnvAdd("mode_threshold", 0.49)
-
-def GetThreshold(local_env):
-    return local_env["mode_threshold"]
+# Here goes data
 
 ################################################################################
 
@@ -23,49 +17,25 @@ def profanity(text):
 
 ################################################################################
 
-tests = [] # (name, queue_in, queue_out, weight)
-processes = [] # reduntant but, who knows
-
-import threading # protection against race in communication with subprocesses. Not sure if it's necessary
-lock = threading.Lock()
-def Lock():
-    global lock
-    lock.acquire()
-def Release():
-    global lock
-    lock.release()
-
-def CheckProcess(qi, qo, func):
-    while True:
-        text = qi.get()
-        result = False
-        try:
-            result = func(text)
-        except:
-            pass
-        qo.put(result)
+tests = [] # (name, func, weight)
 
 # Add check for specific type of hate speech
-def AddCheck(name, func, weight): # bool func(text)
-    qi = Queue()
-    qo = Queue()
-    tests.append((name, qi, qo, weight))
-    p = Process(target=CheckProcess, args=(qi, qo, func,))
-    p.start()
-    processes.append(p)
+def AddTest(name, func, weight): # bool func(text)
+    tests.append( (name, func, weight) )
+
+AddTest("offensive language", profanity, 10)
 
 ################################################################################
 
 # Run tests
-def GetResults(text):
+def RunTests(text): # return [ (name, result, weight) ]
     output = []
-    Lock()
-    for (_1, qi, _q, _2) in tests:
-        qi.put(text)
-    for (name, qi, qo, weight) in tests:
-        result = qo.get()
+    for (name, func, weight) in tests:
+        try:
+            result = func(text)
+        except:
+            result = False
         output.append( (name, result, weight) )
-    Release()
     return output
 
 # Get weighted average
@@ -79,14 +49,10 @@ def GetAverageResults(results):
 
 ################################################################################
 
-def OnInit(bot):
-    AddCheck("offensive language", profanity, 10)
-triggers.OnInitTrigger.append(OnInit)
-
 async def OnMessage(local_env, message, normalised_text):
-    results = GetResults(normalised_text)
+    results = RunTests(normalised_text)
     avg_results = GetAverageResults(results)
-    if avg_results > GetThreshold(local_env):
+    if avg_results > 0:
         await HatefulMessageDetected(local_env, message, normalised_text, results, avg_results)
 triggers.on_message.append(OnMessage)
 
