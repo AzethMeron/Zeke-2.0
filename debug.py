877b0f51c1683283a0c56cf5b849b5827e63ce0d
AzethM <meronentertainment@gmail.com>
2021-09-23 13:30:43 +0200

    New debugging tool

--- debug.py
+++ debug.py
@@ -14,11 +14,17 @@ async def cmd_traceback(ctx, args):
 async def cmd_save(ctx, arg):
     data.SaveGuildEnvironment(ctx.message.guild)
 
+async def cmd_info(ctx, arg):
+    output = str(data.GetGuildEnvironment(ctx.message.guild))
+    for out in tools.segment_text(output, 1980):
+        await ctx.message.reply("```" + out + "```")
+
 ################################################################################
 
 parser = cmd.Parser()
 cmd.Add(parser, "traceback", cmd_traceback, "dummy", "dummy")
 cmd.Add(parser, "save", cmd_save, "dummy", "dummy")
+cmd.Add(parser, "info", cmd_info, "dummy", "dummy")
 
 async def command(ctx, args):
     return await cmd.Parse(parser, ctx, args)
