8eff45e6c9a9636f38ec22bfc3320a609a37ba69
AzethM <meronentertainment@gmail.com>
2021-10-17 00:24:00 +0200

    A little simpler levels script. Still, level formula to be tweaked?

--- levels.py
+++ levels.py
@@ -10,45 +10,52 @@ import tools
 
 ##########################################################################
 
+class LevelData:
+    def __init__(self):
+        self.level = 0
+        self.messages = 0
+        self.experience = 0
+        self.sent_message = False
+
 data.NewGuildEnvAdd("levels verbose", False)
-data.NewUserEnvAdd("level", 0)
-data.NewUserEnvAdd("messages", 0)
-data.NewUserEnvAdd("experience", 0)
-data.NewUserEnvAdd("sent message this minute", False)
+data.NewUserEnvAdd("LevelData", LevelData())
+
+def GetLevelData(user_env):
+    return user_env['LevelData']
 
 ##########################################################################
 
 def get_exp():
     return random.randint(10,25)
 
-def exp_to_next_level(level): # formula to be changed?
+def exp_to_level(level): # formula to be changed?
     return int(70 * (level+1) * math.log(level+2))
 
 ##########################################################################
 
-async def level_up(local_env, message, user_env):
+async def level_up(local_env, message, leveldata):
     if local_env["levels verbose"]:
-        await message.reply(f"Congratulations {message.author.mention}! You've just advanced to {user_env['level']} level.")
+        await message.reply(f"Congratulations {message.author.mention}! You've just advanced to {leveldata.level} level.")
 
 async def on_message(local_env, message, normalised_text):
     user_env = data.GetUserEnvironment(local_env, message.author)
-    user_env["messages"] += 1
-    if not user_env["sent message this minute"]:
-        user_env["sent message this minute"] = True
-        prev_exp = user_env["experience"]
-        next_level_exp = exp_to_next_level(user_env["level"] + 1)
-        user_env["experience"] += get_exp()
-        if prev_exp < next_level_exp and user_env["experience"] >= next_level_exp:
-            user_env["level"] += 1
-            await level_up(local_env, message, user_env)
-
+    leveldata = GetLevelData(user_env)
+    leveldata.messages += 1
+    if not leveldata.sent_message:
+        leveldata.sent_message = True
+        prev_exp = leveldata.experience
+        next_leve_threshold = exp_to_level(leveldata.level + 1)
+        leveldata.experience += get_exp()
+        if leveldata.experience >= next_leve_threshold > prev_exp:
+            leveldata.level += 1
+            await level_up(local_env, message, leveldata)
 triggers.on_message.append(on_message)
 
 async def each_minute(bot, local_env, guild, minute):
     for member in guild.members:
         user_env = data.GetUserEnvironment(local_env, member)
-        if user_env["sent message this minute"]: user_env["sent message this minute"] = False
-
+        leveldata = GetLevelData(user_env)
+        if leveldata.sent_message: leveldata.sent_message = False
 triggers.Timers.append( (1, each_minute) )
 
 ##########################################################################
@@ -56,7 +63,12 @@ triggers.Timers.append( (1, each_minute) )
 async def cmd_list(ctx, args):
     guild = ctx.guild
     local_env = data.GetGuildEnvironment(guild)
-    ulist = [ (member, data.GetUserEnvironment(local_env, member)["level"], data.GetUserEnvironment(local_env, member)["experience"]) for member in guild.members if data.GetUserEnvironment(local_env, member)["level"] >= 1 ]
+    ulist = []
+    for member in guild.members:
+        user_env = data.GetUserEnvironment(local_env, member)
+        leveldata = GetLevelData(user_env)
+        if leveldata.level >= 1:
+            ulist.append( (member, leveldata.level, leveldata.experience) )
     ulist.sort(key = lambda item: -item[1] )
     output = "Level Leaderboard" + "\n"
     for item in ulist:
@@ -66,7 +78,7 @@ async def cmd_list(ctx, args):
     return True
 
 async def cmd_verbose(ctx, args):
-    local_env = data.GetGuildEnvironment(ctx.message.author)
+    local_env = data.GetGuildEnvironment(ctx.message.guild)
     local_env["levels verbose"] = not local_env["levels verbose"]
     await ctx.message.reply(f'Levels verbose: {local_env["levels verbose"]}')
     return True
