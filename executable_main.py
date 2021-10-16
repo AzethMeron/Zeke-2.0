ca1de458cdb8812d2b9543a04db0aecd399c1961
AzethM <meronentertainment@gmail.com>
2021-10-16 03:16:25 +0200

    I don't think that handler works

--- executable_main.py
+++ executable_main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import signal
 import discord # Discord API
 from discord.ext import tasks
 from discord.ext import commands
@@ -177,11 +176,6 @@ async def cmd_debug(ctx, *args):
 ################################################################################
 
 #################################### MAIN ######################################
-
-def term_handler(signum, frame):
-    for guild in DiscordClient.guilds: data.SaveGuildEnvironment(guild)
-
-signal.signal(signal.SIGTERM, term_handler)
 
 @DiscordClient.event
 async def on_ready():
