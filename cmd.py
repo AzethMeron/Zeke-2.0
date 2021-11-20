7bd7dc372f43ab48ea62c7e6190547cd7cbd3242
AzethM <meronentertainment@gmail.com>
2021-11-20 02:02:50 +0100

    More improvements to CMD

--- cmd.py
+++ cmd.py
@@ -13,13 +13,18 @@ def Parser():
     return copy.deepcopy(default_parser)
 
 def Help(parser, args, author):
-    output = str()
+    # Parametried
     if len(args) == 1:
         cmd = args[0]
         if cmd not in parser:
-            raise KeyError(f'{cmd} - command not found')
-        output = output + parser[cmd][2]
-        return output
+            commands = GetSimilarCommands(parser, cmd, author)
+            raise KeyError(f'Command "{cmd}" not found. Did you mean {commands[0]}?')
+        if type(parser[cmd]) == type(Parser()):
+            return Help(parser[cmd], args[1:], author)
+        else:
+            return arser[cmd][2]
+    # Not parametrized
+    output = str()
     for cmd in parser:
         (_, help, _2, perms) = parser[cmd]
         if author.guild_permissions >= perms: output = output + '{0: <10}'.format(cmd) + help + "\n"
@@ -31,6 +36,12 @@ def Add(parser, cmd, func, help, longhelp, perms = discord.Permissions.none()):
         raise KeyError(f'{cmd} already present in parser')
     parser[cmd] = (func, help, longhelp, perms)
 
+def GetSimilarCommands(parser, cmd, author):
+    commands = [ command for command in parser if author.guild_permissions >= parser[command][3] ]
+    commands.append("help")
+    commands.sort(key = lambda x: -fuzz.ratio(x, cmd))
+    return commands
+
 async def Parse(parser, ctx, args):
     try:
         if len(args) == 0: raise KeyError('no command specified. Try "help"')
@@ -41,11 +52,8 @@ async def Parse(parser, ctx, args):
                 await ctx.message.reply("```"+out+"```")
             return True
         if cmd not in parser:
-            commands = [ command for command in parser if ctx.message.author.guild_permissions >= parser[command][3] ]
-            commands.append("help")
-            commands.sort(key = lambda x: -fuzz.ratio(x, cmd))
+            commands = GetSimilarCommands(parser, cmd, ctx.message.author)
             raise KeyError(f'Command "{cmd}" not found. Did you mean {commands[0]}?')
-            return True
         if not ctx.message.author.guild_permissions >= parser[cmd][3]: 
             raise RuntimeError("Insufficent permissions")
         if not await parser[cmd][0](ctx, args):
